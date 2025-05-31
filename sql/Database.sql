6ee8d40c783360a01d423506f6719ad3a26bd0a3
張瀧予 <D1285312@o365.fcu.edu.tw>
2025-06-01 01:51:04 +0800

    Update Database.sql

--- sql/Database.sql
+++ sql/Database.sql
@@ -194,7 +194,7 @@ CREATE TABLE `login_info` (
 --
 
 INSERT INTO `login_info` (`professor_accountnumber`, `professor_password`, `verification_code`, `email`, `teacher_ID`) VALUES
-('P101', 'testpass123', NULL, 'leejs@fcu.edu.tw', NULL);
+('P101', 'testpass123', NULL, 'leejs@fcu.edu.tw', 'T002');
 
 -- --------------------------------------------------------
 
@@ -284,7 +284,7 @@ CREATE TABLE `personal_info` (
 --
 
 INSERT INTO `personal_info` (`teacher_ID`, `teacher_name`, `teacher_email`, `teacher_intro`, `office_location`, `office_hours`) VALUES
-('T002', '李榮三', 'leejs@fcu.edu.tw', '資安領域的先驅，專精人工智慧資安應用，引領未來科技發展。研究成果不僅在學術界備受矚目，更在產業界產生深遠影響。', '資訊安全實驗室', '星期一、四、五 12:10~1300(test)');
+('T002', '李榮三', 'leejs@fcu.edu.tw', '資安領域的先驅，專精人工智慧資安應用，引領未來科技發展。研究成果不僅在學術界備受矚目，更在產業界產生深遠影響。', 'E405(test)', '星期一、四、五 12:10~1300(test)');
 
 -- --------------------------------------------------------
 
